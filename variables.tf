78e8ac5b7b80c9b8e67d0c42c34db673d8316124
Soravis Prommas <soravis.pr@bluebik.com>
2025-11-18 11:30:57 +0700

    test

---
+++ variables.tf
@@ -0,0 +1,14 @@
+variable "project_id" {
+    type= string
+    description = "sandbox-application-478506"
+}
+
+variable "region" {
+    type= string
+    description = "asia-southeast1"
+}
+
+variable  "zone" {
+    type = string
+    description = "asia-southeast1-a"
+}
